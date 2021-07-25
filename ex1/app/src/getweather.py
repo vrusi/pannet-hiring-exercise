d4f804c013eb11bafdace4c5dc10f9c9fad0f19d
vrusi <me@vrusinkova.com>
2021-07-26 00:50:58 +0200

    Add the solution to exercise 1.2

---
+++ ex1/app/src/getweather.py
@@ -0,0 +1,34 @@
+import os
+import pyowm
+from pyowm import OWM
+import sys
+
+""" OWM_API_KEY = os.environ['OWM_API_KEY']
+OWM_CITY = os.environ['OWM_CITY'] """
+
+OWM_API_KEY = '3a7fe26da8afb9fa8efdee83e73404cd'
+OWM_CITY = 'Honolulu'
+
+
+def get_weather():
+    owm = OWM(OWM_API_KEY)
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(OWM_CITY)
+    weather = observation.weather
+
+    return weather
+
+
+def log_weather_info():
+    weather = get_weather()
+    description = weather.detailed_status
+    temperature = weather.temperature('celsius')['temp']
+    humidity = weather.humidity
+    
+    to_log = 'city="' + OWM_CITY + '", description="' + description + '", temp=' + str(temperature) + ', humidity=' + str(humidity)
+    sys.stdout.write(to_log)
+
+
+if __name__ == "__main__":
+   log_weather_info()
